006e82b618c2e71df74f569f25aa5e005413b567
orangeem <orangeem@naver.com>
2016-02-03 00:10:41 -0500

    Add gameController and Polygon Coilider

---
+++ Assets/_Scripts/GameController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+    // PUBLIC INSTANCE VARIABLES
+    public int cactusNumber = 3;
+    public CactusController cactus;
+
+	// Use this for initialization
+	void Start () {
+        this._initialize();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    //Initial Method
+    private void _initialize()
+    {
+
+        for (int cactusCount = 0; cactusCount < this.cactusNumber; cactusCount++)
+        {
+            Instantiate(cactus.gameObject);
+        }
+    }
+}
