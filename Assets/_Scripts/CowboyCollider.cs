685b31dc6f07dfaec270bf033933ad4b953044ce
orangeem <orangeem@naver.com>
2016-02-03 02:05:56 -0500

    Add sounds and collider

---
+++ Assets/_Scripts/CowboyCollider.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CowboyCollider : MonoBehaviour {
+    //Private Instance Variables
+    private AudioSource[] _audioSources;
+    private AudioSource _skeletonSound;
+    private AudioSource _cactusSound;
+
+    //Public Instance Variables
+    public GameController gameController;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Initialize the audioSources array
+        this._audioSources = gameObject.GetComponents<AudioSource>();
+        this._cactusSound = this._audioSources[1];
+        this._skeletonSound = this._audioSources[2];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("skeleton"))
+        {
+            this._skeletonSound.Play();           
+        }
+        if (other.gameObject.CompareTag("cactus"))
+        {
+            this._cactusSound.Play();            
+        }
+    }
+}
